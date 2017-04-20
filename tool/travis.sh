e41e1a112a90f42f2b30eb9aed2c62ba7eebf1fa
Devon Carew <devoncarew@google.com>
2017-04-20 12:16:30 -0700

    add package:logging to the pubspec

--- tool/travis.sh
+++ tool/travis.sh
@@ -12,13 +12,3 @@ dart -c bin/tuneup.dart check
 
 # Run the tests.
 dart -c test/all.dart
-
-# Install dart_coveralls; gather and send coverage data.
-if [ "$REPO_TOKEN" ]; then
-  pub global activate dart_coveralls
-  pub global run dart_coveralls report \
-    --token $REPO_TOKEN \
-    --retry 2 \
-    --exclude-test-files \
-    test/all.dart
-fi
